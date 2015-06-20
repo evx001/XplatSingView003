15e112b450c08f877bb191f09bc95eb7ed61c0f5
evx001 <eviexeris@gmail.com>
2015-06-20 19:58:27 -0400

    Conversation added

---
+++ crossPlatSingView003/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace crossPlatSingView003
+{
+	public class Conversation
+	{
+		public string Id { get; set; }
+
+		public string UserId { get; set; }
+
+		public string Username { get; set; }
+	}
+}
+
