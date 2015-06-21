bbce03a9b2f3bf58c8221126517bc48f62d89ae6
evx001 <eviexeris@gmail.com>
2015-06-20 22:03:25 -0400

    Message modified

--- crossPlatSingView003/Message.cs
+++ crossPlatSingView003/Message.cs
@@ -4,9 +4,15 @@ namespace crossPlatSingView003
 {
 	public class Message
 	{
-		public Message ()
-		{
-		}
+		public string Id { get; set; }
+
+		public string ConversationId { get; set; }
+
+		public string UserId { get; set; }
+
+		public string Username { get; set; }
+
+		public string Text { get; set; }
 	}
 }
 
