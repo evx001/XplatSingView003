bcdb27e32c5880879f65029891af6222ddc2e527
evx001 <eviexeris@gmail.com>
2015-06-20 22:02:35 -0400

    FakeWebUpdated

--- crossPlatSingView003/FakeWebService.cs
+++ crossPlatSingView003/FakeWebService.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace crossPlatSingView003
 {
 	public class FakeWebService
 	{
+		public int SleepDuration { get; set; }
+
 		public FakeWebService ()
 		{
+			SleepDuration = 1; 
+		}
+
+		private Task Sleep ()
+		{ 
+			return Task.Delay (SleepDuration);
+		}
+
+		public async Task<User> Login (string username, string password)
+		{
+			await Sleep ();
+			return new User { Id = "1", Username = username }; 
 		}
 	}
 }
