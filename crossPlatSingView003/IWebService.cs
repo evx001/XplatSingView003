663313d3ddc98913c435c10b1203faef16565da3
evx001 <eviexeris@gmail.com>
2015-06-20 20:30:32 -0400

    IwebService

---
+++ crossPlatSingView003/IWebService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace crossPlatSingView003
+{
+	public interface IWebService
+	{
+		Task<User> Login (string username, string password);
+
+		Task<User> Register (User user);
+
+		Task<User [] > GetFriends (string userId);
+
+		Task<User> AddFriend (string userId);
+
+		Task<Conversation [] > GetCoversations (string username);
+
+		Task<Message [] > GetMessages (string conversationId);
+
+		Task<Message> SendMessage (Message message);
+	}
+}
+
---
+++ crossPlatSingView003/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crossPlatSingView003
+{
+	public class Message
+	{
+		public Message ()
+		{
+		}
+	}
+}
+
