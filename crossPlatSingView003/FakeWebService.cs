18e5a393f21db121a02de698471ce5f75a9d9f81
evx001 <eviexeris@gmail.com>
2015-06-21 19:25:48 -0400

    Fake serve async

--- crossPlatSingView003/FakeWebService.cs
+++ crossPlatSingView003/FakeWebService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Org.Apache.Http.Authentication;
+using crossPlatSingView003;
 
 namespace crossPlatSingView003
 {
@@ -22,6 +24,42 @@ namespace crossPlatSingView003
 			await Sleep ();
 			return new User { Id = "1", Username = username }; 
 		}
+		public async Task<User> Register(User user){
+			await Sleep (); 
+			return user; 
+		}
+		public async Task<User[]> GetFriends (string userId){
+			await Sleep ();
+			return new [] {
+				new User { Id = "2", Username = "JAdams" }, 
+				new User { Id = "3", Username = "TJefferson" }, 
+				new User { Id = "4", Username = "GWashington" },
+			}; 
+		}
+		public async Task<User> AddFriend(string userId, string username){
+			await Sleep (); 
+			return new User { Id = "5", } Username = username }; 
+		}
+	public async Task<Conversation[]> 
+	GetConversations(string userId){
+		await Sleep(); 
+		return new [] {
+			new Conversation { Id = "1", UserId = "2" },
+			new Conversation { Id = "2 ", UserId = "3"},
+			new Conversation { Id = "3 ", UserId = "4"},
+		};
 	}
-}
+	public async Task<Message[]> GetMessage(int conversationId){
+		await Sleep(); 
+		return new [] { 
+			new Message {
+				Id = "1", 
+				ConversationId = conversationId, 
+				UserId = "2", 
+				Text = "Hey",
+			}
+			}
+		}
+	}// class
+} // namespace
 
