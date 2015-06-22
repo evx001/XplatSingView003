01da56f2e684d751aa1fe8c07cec4c8f943c2f25
evx001 <eviexeris@gmail.com>
2015-06-21 23:01:57 -0400

    pub async and ToString -> compiles

--- crossPlatSingView003/FakeWebService.cs
+++ crossPlatSingView003/FakeWebService.cs
@@ -11,55 +11,93 @@ namespace crossPlatSingView003
 
 		public FakeWebService ()
 		{
-			SleepDuration = 1; 
+			SleepDuration = 1;
 		}
 
 		private Task Sleep ()
-		{ 
+		{
 			return Task.Delay (SleepDuration);
 		}
 
 		public async Task<User> Login (string username, string password)
 		{
 			await Sleep ();
-			return new User { Id = "1", Username = username }; 
+			return new User { Id = "1", Username = username };
 		}
-		public async Task<User> Register(User user){
-			await Sleep (); 
-			return user; 
+
+		public async Task<User> Register (User user)
+		{
+			await Sleep ();
+			return user;
 		}
-		public async Task<User[]> GetFriends (string userId){
+
+		public async Task<User[]> GetFriends (string userId)
+		{
 			await Sleep ();
 			return new [] {
-				new User { Id = "2", Username = "JAdams" }, 
-				new User { Id = "3", Username = "TJefferson" }, 
+				new User { Id = "2", Username = "JAdams" },
+				new User { Id = "3", Username = "TJefferson" },
 				new User { Id = "4", Username = "GWashington" },
-			}; 
+			};
 		}
-		public async Task<User> AddFriend(string userId, string username){
-			await Sleep (); 
-			return new User { Id = "5", } Username = username }; 
-		}
-	public async Task<Conversation[]> 
-	GetConversations(string userId){
-		await Sleep(); 
-		return new [] {
-			new Conversation { Id = "1", UserId = "2" },
-			new Conversation { Id = "2 ", UserId = "3"},
-			new Conversation { Id = "3 ", UserId = "4"},
-		};
-	}
-	public async Task<Message[]> GetMessage(int conversationId){
-		await Sleep(); 
-		return new [] { 
-			new Message {
-				Id = "1", 
-				ConversationId = conversationId, 
-				UserId = "2", 
-				Text = "Hey",
-			}
-			}
-		}
-	}// class
-} // namespace
 
+		public async Task<User> AddFriend (string userId, string username)
+		{
+			await Sleep ();
+			return new User { Id = "5",  Username = username };
+		}
+
+		public async Task<Conversation[]>
+		GetConversations (string userId)
+		{
+			await Sleep ();
+			return new [] {
+				new Conversation { Id = "1", UserId = "2" },
+				new Conversation { Id = "2 ", UserId = "3" },
+				new Conversation { Id = "3 ", UserId = "4" },
+			};
+		}
+
+		public async Task<Message[]> GetMessage (int
+			conversationId)
+		{
+			await Sleep ();
+			////////// multiple ToString conversions //////////////////
+			return new [] {
+				new Message {
+					Id = "1",
+					ConversationId = conversationId.ToString (),
+					UserId = "2",
+					Text = "Hey",
+				},
+				new Message {
+					Id = "2",
+					ConversationId = conversationId.ToString (),
+					UserId = "1",
+					Text = "What's Up?",
+				},
+				new Message {
+					Id = "3",
+					ConversationId = conversationId.ToString (),
+					UserId = "2",
+					Text = "Have you seen Fury Road yet?",
+				},
+				new Message {
+					Id = "4",
+					ConversationId = conversationId.ToString (),
+					UserId = "1",
+					Text = "It's Awesome!!!",
+				},
+			};
+		}
+
+		public async Task<Message> SendMessage (Message message)
+		{
+			await Sleep ();
+			return message;
+		}
+
+		/*class end */
+	}
+	/* namespaceEnd*/
+}
