351431de86d1385d031489ee6ef13ce2576b58a8
evx001 <eviexeris@gmail.com>
2015-06-22 02:33:50 -0400

    Interface Settings

---
+++ crossPlatSingView003/ISettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+
+namespace crossPlatSingView003
+{
+	public interface ISettings
+	{
+		User User { get; set; }
+
+		void Save ();
+	}
+}
+
